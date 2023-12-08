f6e8df068e08246589503344dac3b3f53eee9a53
Kevin Martinez <kevin.mtzv23@gmail.com>
2023-12-08 04:38:51 -0600

    webapp

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,67 @@
+import React from 'react';
+//import { products } from '../products';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Axios from 'axios';
+
+const imagenes = require.context('../assets', true);
+
+
+export const ProductList = ({allProducts, setAllProducts, countProducts, setCountProducts, total, setTotal}) => {
+
+    const [products, setProducts] = useState([]);
+
+
+    const getProducts = () => {
+        Axios.get("http://service-backend/licores").then((response) => {
+            setProducts(response.data);
+            console.log(response.data[0].image_url);
+        })
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+   
+    const onAddProduct = (product) => {
+
+        if (allProducts.find(item => item.licor_id === product.licor_id)) {
+            const products = allProducts.map(item => item.licor_id === product.licor_id 
+                ? {...item, quantity: item.quantity + 1}
+                : item
+            );
+
+            setTotal(total + product.price * product.quantity);
+            setCountProducts(countProducts + product.quantity)
+            return setAllProducts([...products]);
+        }
+
+        setTotal(total + product.price * product.quantity);
+        setCountProducts(countProducts + product.quantity);
+        setAllProducts([...allProducts, product]);
+        
+    }
+
+
+    return (
+        
+        <div className='container-items'>
+            {   
+                
+                products.map( product => (
+                    <div className='item' key={product.licor_id}>
+                        <figure>
+					        <img src = {imagenes(`./${product.image_url}`)} alt = {product.product_name}/>
+				        </figure>
+                        <div className="info-product">
+                            <h2>{product.product_name}</h2>
+                            <p className="price">${product.price}</p>
+                            <button className="btn-add-cart" onClick={() => onAddProduct(product)}>Añadir al carrito</button>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
